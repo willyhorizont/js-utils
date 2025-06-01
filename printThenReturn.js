acfdadefe8fa9406b482492815476ee16227da70
willyhorizont <willyhorizont@gmail.com>
2025-06-02 00:48:41 +0700

    update printThenReturn

--- printThenReturn.js
+++ printThenReturn.js
@@ -1,3 +1,3 @@
-const printThenReturn = (anything) => [console.log(anything), anything][0];
+const printThenReturn = (anything) => [console.log(anything), anything][1];
 
 export { printThenReturn };
